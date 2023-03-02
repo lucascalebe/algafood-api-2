4a69f1603d7db6a61ac4e5207c32f6a3ff9cbee4
Lucas Calebe <lucascalebe07@hotmail.com>
2023-03-02 17:20:29 -0300

    :tada: create project

---
+++ src/test/java/com/algaworks/algafoodapi2/AlgafoodApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApi2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
