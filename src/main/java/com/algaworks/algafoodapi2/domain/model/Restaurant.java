c2ee87a15852ee23005b409544f79e0f28ad4b98
Lucas Calebe <lucascalebe07@hotmail.com>
2023-04-28 14:58:33 -0400

    :sparkles: add kitchen to restaurant

--- src/main/java/com/algaworks/algafoodapi2/domain/model/Restaurant.java
+++ src/main/java/com/algaworks/algafoodapi2/domain/model/Restaurant.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 
 @Data
@@ -25,4 +26,7 @@ public class Restaurant {
 
   @Column
   private BigDecimal shippingFee;
+
+  @ManyToOne
+  private Kitchen kitchen;
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into kitchen (name) values ('Thai');
-insert into kitchen (name) values ('Indian');
+insert into kitchen (id, name) values (1, 'Thai');
+insert into kitchen (id, name) values (2, 'Indian');
 
-insert into restaurant (name, shipping_fee) values ('Thai Gourmet', 10);
-insert into restaurant (name, shipping_fee) values ('Thai Delivery', 9.50);
-insert into restaurant (name, shipping_fee) values ('Tuk Tuk Comida Indiana', 15);
+insert into restaurant (name, shipping_fee, kitchen_id) values ('Thai Gourmet', 10, 1);
+insert into restaurant (name, shipping_fee, kitchen_id) values ('Thai Delivery', 9.50, 1);
+insert into restaurant (name, shipping_fee, kitchen_id) values ('Tuk Tuk Comida Indiana', 15, 2);
