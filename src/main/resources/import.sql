eb269d4a9773988768a9526924cc966df6048b0f
Lucas Calebe <lucascalebe07@hotmail.com>
2023-04-28 14:32:21 -0400

    :hammer: add restaurant to script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into kitchen (name) values ('Thai');
 insert into kitchen (name) values ('Indian');
+
+insert into restaurant (name, shipping_fee) values ('Thai Gourmet', 10);
+insert into restaurant (name, shipping_fee) values ('Thai Delivery', 9.50);
+insert into restaurant (name, shipping_fee) values ('Tuk Tuk Comida Indiana', 15);
