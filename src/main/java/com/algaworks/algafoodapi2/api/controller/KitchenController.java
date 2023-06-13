ce5f1a848eb338ce76ebadf2d96c69548b33fee4
Lucas Calebe <lucascalebe07@hotmail.com>
2023-06-13 09:02:29 -0400

    :sparkles: 4.23 create kitchen post

--- src/main/java/com/algaworks/algafoodapi2/api/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafoodapi2/api/controller/KitchenController.java
@@ -3,10 +3,15 @@ package com.algaworks.algafoodapi2.api.controller;
 import com.algaworks.algafoodapi2.domain.model.Kitchen;
 import com.algaworks.algafoodapi2.domain.repository.KitchenRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,10 +29,19 @@ public class KitchenController {
   }
 
   @GetMapping("/{kitchenId}")
-  public Kitchen kitchen(@PathVariable Long kitchenId) {
-    return kitchenRepository.findById(kitchenId)
-            .orElseThrow(() -> new IllegalArgumentException(
-                    String.format("kitchen of id %s was not found", kitchenId))
-            );
+  public ResponseEntity<Kitchen> getKitchen(@PathVariable Long kitchenId) {
+    var kitchen = kitchenRepository.findById(kitchenId);
+
+    if (kitchen.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(kitchen.get());
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Kitchen addKitchen(@RequestBody Kitchen kitchen) {
+    return kitchenRepository.save(kitchen);
   }
 }
